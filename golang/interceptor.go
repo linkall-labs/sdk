4b284e8d5ac31f802a2bce5b23719ed9cba6f356
James Yin <ywhjames@hotmail.com>
2023-05-23 11:32:04 +0800

    fix: return nil from error interceptor
    Signed-off-by: James Yin <ywhjames@hotmail.com>

--- golang/interceptor.go
+++ golang/interceptor.go
@@ -1,17 +1,22 @@
 package vanus
 
 import (
+	// standard libraries.
 	"context"
 
+	// third-party libraries.
 	"google.golang.org/grpc"
 
+	// first-party libraries.
 	"github.com/vanus-labs/vanus/pkg/errors"
 )
 
 func UnaryClientInterceptor() grpc.UnaryClientInterceptor {
-	return func(ctx context.Context, method string, req interface{}, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	return func(ctx context.Context, method string, req interface{}, reply interface{},
+		cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption,
+	) error {
 		err := invoker(ctx, method, req, reply, cc, opts...)
-		if et, ok := errors.FromError(err); ok {
+		if et, ok := errors.FromError(err); ok && et != nil {
 			return et
 		}
 		return err
